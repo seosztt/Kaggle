2bf1fec8d28e9786bb5838e25367159d466749dc
seosztt <seosztt@gmail.com>
2021-09-08 01:31:13 +0900

    work 4

---
+++ house_prices/house_prices.py
@@ -0,0 +1,114 @@
+# 데이터 불러오기
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+    #train_link = "https://bit.ly/38Lia2e"
+    #test_link = "https://bit.ly/3h5eqNL"
+    #train1 = pd.read_csv(train_link)
+    #test1 = pd.read_csv(test_link)
+
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+train1 = pd.read_csv('data/train.csv')
+test1 = pd.read_csv('data/test.csv') 
+
+
+
+# 전처리
+## 전처리 시작
+### 필요한 함수 정의
+def null_count(dataset):
+    '''
+    해당 데이터셋에 결측치가 있는 feature와 개수 출력
+    '''
+    return dataset.isnull().sum()[dataset.isnull().sum()!=0]
+    
+def null_check(feature):
+    '''
+    해당 feature의 결측치가 각 데이터셋에 얼마나 있는지 확인.
+    dataset도 input으로 받아야 할지 고민.
+    '''
+    print('--', feature, '--')
+    print('train :', train[feature].isnull().sum())
+    print('test :', test[feature].isnull().sum())
+
+def val_list(dataset, feature):
+    '''
+    해당 dataset의 feature가 가진 value들 중복 없이 리스트로 출력
+    '''
+    return list(set(dataset[feature].values.tolist()))
+
+#val_list = set(train['MSZoning'].values.tolist()) - 중복 없이 value종류 추출 
+# 근데 이건 순서가 내 맘대로 안됨
+
+def val_count(feature):
+    '''
+    train, test 데이터의 value_count를 같이 출력
+    input에 데이터셋도 받는게 좋을지..?
+    '''
+    print('-----', feature, '-----')
+    print('---- train ----')
+    print(train[feature].value_counts())
+    print('---- test ----')
+    print(test[feature].value_counts(),'\n')
+
+def map_order(dataset, feature, val_list):
+    '''
+    Value들을 0부터 순서대로 매핑
+    데이터셋(train/test), feature명, 원하는 순서대로 value_list 입력
+    '''
+    mapping = dict(zip(val_list, range(len(val_list))))
+    return dataset[feature].map(mapping) #원하는 순서대로 0부터 매핑된 컬럼 출력
+
+
+def val_cat(dataset, feature):
+    '''
+    dataset, feature를 입력받아서 해당 feature에 대해 categorize
+    '''
+    val_list = set(dataset[feature].values.tolist())
+    for feat in val_list:
+        dataset[feat] = (dataset[feature]==feat).astype(int)
+    dataset = dataset.drop(feature, axis = 1)
+    
+def avg_price(feature):
+    '''
+    train set 내의 해당 feature의 value들에 따른 가격의 평균
+    '''
+    for feat in set(train[feature].values.tolist()):
+        mean_price = train['SalePrice'][train[feature] == feat].mean()
+        print(feat, ':', mean_price)
+        
+def price_plot(feature):
+    '''
+    train set 내의 해당 feature의 value에 따른 가격의 평균 plot 그래프로 출력
+    '''
+    train.groupby(feature).mean()['SalePrice'].plot()
+
+def brief():
+    '''
+    feature의 분류를 브리핑
+    '''
+    print('그대로 넣을 거 {}개: \n'.format(len(feature_raw)),feature_raw,'\n')
+    print('모아놓고 그대로 넣을지? one-hot을 할지 등 {}개: \n'.format(len(feature_dis)),feature_dis,'\n')
+    print('매핑 할 데이터 {}개: \n'.format(len(feature_map)),feature_map,'\n')
+    print('one-hot encoding 시킬 것 {}개: \n'.format(len(feature_onehot)),feature_onehot,'\n')
+    print('따로 봐야 할 것 {}개: \n'.format(len(feature_extra)),feature_extra,'\n')
+    print('삭제할 것 {}개: \n'.format(len(feature_del)),feature_del,'\n')
+
+def price_cor(feature):
+    '''
+    feature와 집값 사이의 상관계수를 반환
+    '''
+    return round(float(pd.DataFrame(train.corr()['SalePrice']).loc[feature]),3)
+
+### feature 분류
+feature_raw = []
+feature_dis = []
+feature_map = []
+feature_onehot = []
+feature_extra = []
+feature_del = []
+
+## Feature들 살펴보기
